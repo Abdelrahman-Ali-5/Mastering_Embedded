ca974f788d8948687fc7c7f5cfc19ba757456b99
Abdelrahman-Ali-5 <abdelrahmanelkollaly@gmail.com>
2023-10-02 00:36:02 +0300

    lab1 with make file

--- Unit 3 Embedded C/Lab1 with Make/app.c
+++ Unit 3 Embedded C/Lab1 with Make/app.c
@@ -1,6 +1,8 @@
 #include "uart.h"
 unsigned char string_buff[100] = "learn_in_depth_kollaly" ;
 unsigned char const string2[100] = "create_ro_data" ;
+unsigned char const string3[100] = "change only this file" ;
+
 
 void main(void){
 	
